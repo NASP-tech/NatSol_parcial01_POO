3ac9de5e83a3bee40f44930b3f8a05619a62aa54
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-17 09:51:23 -0600

    Empleado
    Empleado

--- Parcial01/src/com/NASP/x00120319/Empleado.java
+++ Parcial01/src/com/NASP/x00120319/Empleado.java
@@ -1,5 +1,29 @@
 package com.NASP.x00120319;
 
 public abstract class Empleado {
-    protected 
+    protected String nombre;
+    protected String puesto;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getPuesto(){
+        return puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salary){
+        this.salario = salary;
+    }
 }
