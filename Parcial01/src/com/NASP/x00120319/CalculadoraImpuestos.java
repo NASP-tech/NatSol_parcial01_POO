f0021c1f824713f0957dad64ae73dac0707eadf4
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-17 10:59:26 -0600

    CalculadoraImpuestos
    CalculadoraImpuestos

--- Parcial01/src/com/NASP/x00120319/CalculadoraImpuestos.java
+++ Parcial01/src/com/NASP/x00120319/CalculadoraImpuestos.java
@@ -1,4 +1,23 @@
 package com.NASP.x00120319;
 
-public class CalculadoraImpuestos {
+public final class CalculadoraImpuestos{
+    private double totalRenta;
+    private double totalISSS;
+    private double totalAFP;
+
+    private CalculadoraImpuestos(double totalRenta, double totalISSS, double totalAFP) {
+        this.totalRenta = totalRenta;
+        this.totalISSS = totalISSS;
+        this.totalAFP = totalAFP;
+    }
+
+    public double calcularPago(Empleado e){
+        double total = 0;
+        return total;
+
+    }
+
+    public String mostrarTotales(){
+        return "El total de los impuesto fue de: $" + totalRenta + totalISSS + totalAFP;
+    }
 }
