3d6bccafba3f2cb3543909e33ec3367d9136a246
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-17 10:28:42 -0600

    PlazaFija
    PlazaFija

--- Parcial01/src/com/NASP/x00120319/PlazaFija.java
+++ Parcial01/src/com/NASP/x00120319/PlazaFija.java
@@ -1,4 +1,18 @@
 package com.NASP.x00120319;
 
-public class PlazaFija {
+public class PlazaFija extends Empleado{
+    private int extension;
+
+    public PlazaFija(String nombre, String puesto, double salario, int extension) {
+        super(nombre, puesto, salario);
+        this.extension = extension;
+    }
+
+    public int getExtension(){
+        return extension;
+    }
+
+    public void setExtension(int telefono){
+        this.extension = telefono;
+    }
 }
