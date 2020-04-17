300d519f684cf613019be1d54da052ae9479bd7f
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-17 10:24:27 -0600

    ServicioProfesional
    ServicioProfesional

--- Parcial01/src/com/NASP/x00120319/ServicioProfesional.java
+++ Parcial01/src/com/NASP/x00120319/ServicioProfesional.java
@@ -1,4 +1,18 @@
 package com.NASP.x00120319;
 
-public class ServicioProfesional {
+public class ServicioProfesional extends Empleado{
+    private int mesesContrato;
+
+    public ServicioProfesional(String nombre, String puesto, double salario, int mesesContrato) {
+        super(nombre, puesto, salario);
+        this.mesesContrato = mesesContrato;
+    }
+
+    public int getMesesContrato(){
+        return mesesContrato;
+    }
+
+    public void setMesesContrato(int meses){
+        this.mesesContrato = meses;
+    }
 }
