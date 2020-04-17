d2bf4d11a3b9d839b289f997471b8b640c841434
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-17 14:04:45 -0600

    Add: NotFoundExceptions
    Add: NotFoundExceptions

--- Parcial01/src/com/NASP/x00120319/NotFoundException.java
+++ Parcial01/src/com/NASP/x00120319/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.NASP.x00120319;
 
-public class NotFoundException extends Exception{
+public class NotFoundException extends Exception {
     public NotFoundException(String message) {
         super(message);
     }
