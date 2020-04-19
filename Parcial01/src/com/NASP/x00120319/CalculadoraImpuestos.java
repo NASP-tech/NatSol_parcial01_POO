49128a6d5360eb50f972bef994ead47a979dcda3
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-18 22:44:02 -0600

    Add: CalculadoraImpuestos
    Add: CalculadoraImpuestos

--- Parcial01/src/com/NASP/x00120319/CalculadoraImpuestos.java
+++ Parcial01/src/com/NASP/x00120319/CalculadoraImpuestos.java
@@ -1,25 +1,61 @@
 package com.NASP.x00120319;
 
 public final class CalculadoraImpuestos{
-    private double totalRenta;
-    private double totalISSS;
-    private double totalAFP;
+    private static double totalRenta;
+    private static double totalISSS;
+    private static double totalAFP;
     private double total;
 
-    private CalculadoraImpuestos(double totalRenta, double totalISSS, double totalAFP) {
-        this.totalRenta = totalRenta;
-        this.totalISSS = totalISSS;
-        this.totalAFP = totalAFP;
+    private CalculadoraImpuestos(){}
+
+    public static double calcularPago(Empleado e){
+        double salario, AFP = 0, renta = 0, ISSS = 0, restante = 0;
+        salario = e.getSalario();
+
+        if(e instanceof ServicioProfesional) {
+            renta = e.getSalario()*0.1;
+            totalRenta += renta;
+            salario -= renta;
+            //salario -= e.getSalario()-renta;
+        }else {
+            AFP = e.getSalario()*0.625;
+            totalAFP += AFP;
+            ISSS = e.getSalario()*0.3;
+            totalISSS += ISSS;
+            restante = e.getSalario()-AFP-ISSS;
+
+            //Haciendo uso del Diagrama de Descuentos DFD
+            if(restante >= 0 && restante <= 472)
+                salario -= restante-renta;
+                //salario -= e.getSalario()-restante;
+            else if(restante >= 472 && restante <= 895){
+                renta = 0.1*(restante-472) + 17.67;
+                totalRenta += renta;
+                salario -= restante-renta;
+                //salario -= e.getSalario()-restante-renta;
+            }
+            else if(restante >= 895 && restante <= 2038){
+                renta = 0.2*(restante-895.24)+60;
+                totalRenta += renta;
+                salario -= restante-renta;
+                //salario -= e.getSalario()-restante-renta;
+            }
+            else {
+                renta = 0.2*(restante-895.24)+60;
+                totalRenta += renta;
+                salario -= restante-renta;
+                //salario -= e.getSalario()-restante-renta;
+            }
+
+        }
+        /*double total = totalAFP+ totalISSS + totalRenta;*/
+        return salario;
+
     }
 
-    public double calcularPago(Empleado e){
-        double total = totalAFP+ totalISSS + totalRenta;
-        return total;
-
-    }
-
-    public String mostrarTotales(){
-        return "El total de los impuesto fue de: $" + total;
+    public static String mostrarTotales(){
+        return "El total de los impuesto fue de: " + "\n\t-> AFP= $" + totalAFP +
+                "\n\t-> ISSS= $" + totalISSS + "\n\t-> Renta= $" + totalRenta;
     }
 
 
